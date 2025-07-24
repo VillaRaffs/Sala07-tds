a6ad2d37462330d9d1116c90e385d46f2831c5ed
Rafaela Villatore <morango20n1@gmail.com>
2025-07-24 08:04:02 -0300

    Add files via upload

--- exercicio01/script.js
+++ exercicio01/script.js
@@ -34,13 +34,30 @@ function divisao(){
 }
 }
 
-function media(){
-  var numero9 = parseFloat(document.getElementById("n9").value)
-    var numero10 = parseFloat(document.getElementById("n10").value)
-    var numero11 = parseFloat(document.getElementById("n11").value)
-    var numero12 = parseFloat(document.getElementById("n12").value)
-    var numero13 = parseFloat(document.getElementById("n13").value)
-    console.log(numero9, numero10, numero11, numero12, numero13)
-    var resp = document.getElementById("respostamedia")
-    resp.textContent = (numero9 + numero10 + numero11 + numero12 + numero13) / 5
+//function media(){
+  //var numero9 = parseFloat(document.getElementById("n9").value)
+    //var numero10 = parseFloat(document.getElementById("n10").value)
+    //var numero11 = parseFloat(document.getElementById("n11").value)
+    //var numero12 = parseFloat(document.getElementById("n12").value)
+    //var numero13 = parseFloat(document.getElementById("n13").value)
+    //console.log(numero9, numero10, numero11, numero12, numero13)
+    //var resp = document.getElementById("respostamedia")
+   // resp.textContent = (numero9 + numero10 + numero11 + numero12 + numero13) / 5
+//}
+//outra maneira de calcular a media
+//arrow function
+const media= () =>{
+var nota1 = parseFloat(document.getElementById('n9').value) 
+var nota2 = parseFloat(document.getElementById('n10').value)
+var nota3 = parseFloat(document.getElementById('n11').value) 
+var nota4 = parseFloat(document.getElementById('n12').value) 
+var nota5 = parseFloat(document.getElementById('n13').value) 
+var media = document.getElementById('media') 
+media.innerHTML = (nota1 + nota2 + nota3 + nota4 + nota5)/5
+var resposta  = (nota1 + nota2 + nota3 + nota4 + nota5)/5
+if(resposta >= 7){
+media.style.color= 'green'
+}else{
+media.style.color ='red'
+}
 }
